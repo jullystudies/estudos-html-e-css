13ad2506739e210a6d998512dd01f54ebb753697
Jullyane Amaral <jullyanea.amaral@gmail.com>
2023-01-21 20:51:52 -0700

    21/01/23

--- NLW SETUP - Rocketseat/script.js
+++ NLW SETUP - Rocketseat/script.js
@@ -1,11 +1,26 @@
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-18", '01-20', '01-21', '01-22', '01-3'],
-  takePills: ['01-02'],
-  journal: ['01-19']
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso! ❌")
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
